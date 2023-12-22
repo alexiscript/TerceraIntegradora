274b3e7521601bb064e2c79b09a231cff387228e
alexiScript <alexiscript@gmail.com>
2023-12-21 21:32:32 -0300

    primer commit

---
+++ src/middlewares/isPremium.middleware.js
@@ -0,0 +1,21 @@
+import SessionDTO from '../dto/UsersDTO/session.dto.js'
+import CustomError from '../utils/errors/customError.js'
+import { ErrorsCause, ErrorsMessage, ErrorsName } from '../utils/errors/errors.enums.js'
+
+
+const isPremium = (req,res,next) =>{
+    const session = new SessionDTO(req.session)
+    const { role } = session
+    if(role === 'premium'){
+        console.log(role)
+        next()
+    } else {
+        CustomError.createCustomError({
+            name: ErrorsName.INVALID_ROLE,
+            cause: ErrorsCause.INVALID_ROLE,
+            message: ErrorsMessage.INVALID_ROLE,
+        })
+    }
+}
+
+export default isPremium
